2a05d699d8550129c595155164640a7a5d96f000
Eric Chrobak <eric@ericchrobak.com>
2018-05-22 14:34:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,15 @@ class Dog
   attr_accessor :name
 
   @@all
-  
+
   def initialize(name)
     @name = name
-    @@all << @name
+    @@all << self.name
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog
+    end
   end
 end
